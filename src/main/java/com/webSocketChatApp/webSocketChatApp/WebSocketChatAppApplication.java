f5d4adac359e98b11f369389819eb5125070dae0
2003Vandu <2003Vandu>
2025-11-26 19:44:19 +0530

    Add AtlisDb

--- src/main/java/com/webSocketChatApp/webSocketChatApp/WebSocketChatAppApplication.java
+++ src/main/java/com/webSocketChatApp/webSocketChatApp/WebSocketChatAppApplication.java
@@ -2,7 +2,7 @@ package com.webSocketChatApp.webSocketChatApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.logging.Logger;
+
 
 @SpringBootApplication
 public class WebSocketChatAppApplication {
